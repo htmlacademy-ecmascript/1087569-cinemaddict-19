0330bb1f3027b22fb4cca276943d663f1773dd0f
SneG95 <Dima6233@yandex.ru>
2023-03-14 16:34:11 +0300

    Добавит модель комментариев

---
+++ src/model/comments-model.js
@@ -0,0 +1,34 @@
+import Observable from '../framework/observable.js';
+import { commentItems } from '../mock/film.js';
+
+export default class CommentsModel extends Observable {
+  #comments = [...commentItems];
+
+  get comments () {
+    return this.#comments;
+  }
+
+  addComment(updateType, update) {
+    this.#comments = [
+      update,
+      ...this.#comments
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  deleteTask(updateType, update) {
+    const index = this.#comments.findIndex((comment) => comment.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting task');
+    }
+
+    this.#comments = [
+      ...this.#comments.slice(0, index),
+      ...this.#comments.slice(index + 1)
+    ];
+
+    this._notify(updateType);
+  }
+}
