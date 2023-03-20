3d4ca614b89a371f29348278c252bf31e3e618fb
SneG95 <Dima6233@yandex.ru>
2023-03-20 17:25:30 +0300

    Реализует паттерн Адаптер

--- src/api/films-api-service.js
+++ src/api/films-api-service.js
@@ -11,12 +11,44 @@ export default class FilmsApiService extends ApiService {
     const response = await this._load({
       url: `movies/${movie.id}`,
       method: Method.PUT,
-      body: JSON.stringify(movie),
+      body: JSON.stringify(this.#adaptToServer(movie)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToServer(film) {
+    const adaptedFilm = {
+      ...film,
+      'film_info': {
+        ...film.filmInfo,
+        'age_rating': film.filmInfo.ageRating,
+        'alternative_title': film.filmInfo.alternativeTitle,
+        'total_rating': film.filmInfo.totalRating,
+        'release': {
+          ...film.filmInfo.release,
+          'release_country': film.filmInfo.release.releaseCountry
+        }
+      },
+      'user_details': {
+        ...film.userDetails,
+        'already_watched': film.userDetails.alreadyWatched,
+        'watching_date': film.userDetails.watchingDate
+      }
+    };
+
+    delete adaptedFilm.filmInfo;
+    delete adaptedFilm.userDetails;
+    delete adaptedFilm.filmInfo.ageRating;
+    delete adaptedFilm.filmInfo.alternativeTitle;
+    delete adaptedFilm.filmInfo.totalRating;
+    delete adaptedFilm.filmInfo.release.releaseCountry;
+    delete adaptedFilm.userDetails.alreadyWatched;
+    delete adaptedFilm.userDetails.watchingDate;
+
+    return adaptedFilm;
   }
 }
--- src/model/films-model.js
+++ src/model/films-model.js
@@ -11,7 +11,9 @@ export default class FilmsModel extends Observable {
     super();
     this.#filmsApiService = filmsApiService;
 
-    this.#filmsApiService.films.then((films) => console.log(films));
+    this.#filmsApiService.films.then((films) => {
+      console.log(this.#adaptToClient(films[0]));
+    });
   }
 
   get films() {
@@ -32,5 +34,37 @@ export default class FilmsModel extends Observable {
     ];
 
     this._notify(updateType, update);
+  }
+
+  #adaptToClient(film) {
+    const adaptedFilm = {
+      ...film,
+      filmInfo: {
+        ...film.film_info,
+        ageRating: film['film_info']['age_rating'],
+        alternativeTitle: film['film_info']['alternative_title'],
+        totalRating: film['film_info']['total_rating'],
+        release: {
+          ...film.film_info.release,
+          releaseCountry: film['film_info']['release']['release_country']
+        }
+      },
+      userDetails: {
+        ...film.user_details,
+        alreadyWatched: film['user_details']['already_watched'],
+        watchingDate: film['user_details']['watching_date']
+      }
+    };
+
+    delete adaptedFilm['film_info'];
+    delete adaptedFilm['user_details'];
+    delete adaptedFilm.filmInfo['age_rating'];
+    delete adaptedFilm.filmInfo['alternative_title'];
+    delete adaptedFilm.filmInfo['total_rating'];
+    delete adaptedFilm.filmInfo['release']['release_country'];
+    delete adaptedFilm.userDetails['already_watched'];
+    delete adaptedFilm.userDetails['watching_date'];
+
+    return adaptedFilm;
   }
 }
